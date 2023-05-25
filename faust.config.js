db68db788284ce1d892296578c90a0f29b2569d3
Jason Bahl <jasonbahl@mac.com>
2023-05-25 13:21:39 -0600

    - add persisted queries plugin - install crypto-hash (for persisted queries plugin)

--- faust.config.js
+++ faust.config.js
@@ -1,12 +1,13 @@
 import { setConfig } from "@faustwp/core";
 import templates from "@/wp-templates/index.js";
 import possibleTypes from "./possibleTypes.json";
+import PersistedQueriesPlugin from "./plugins/PersistedQueriesPlugin";
 
 /**
  * @type {import('@faustwp/core').FaustConfig}
  **/
 export default setConfig({
   templates,
-  experimentalPlugins: [],
+  experimentalPlugins: [new PersistedQueriesPlugin()],
   possibleTypes,
 });
