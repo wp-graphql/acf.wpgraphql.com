518e9cb447c71bcba2804a30fa261b4a6e544f61
Jason Bahl <jasonbahl@mac.com>
2023-06-27 14:50:33 -0600

    - add TempPersistedQueriesFixPlugin

--- faust.config.js
+++ faust.config.js
@@ -2,12 +2,16 @@ import { setConfig } from "@faustwp/core";
 import templates from "@/wp-templates/index.js";
 import possibleTypes from "./possibleTypes.json";
 import PersistedQueriesPlugin from "./plugins/PersistedQueriesPlugin";
+import TempPersistedQueriesFixPlugin from "./plugins/TempPersistedQueriesFixPlugin";
 
 /**
  * @type {import('@faustwp/core').FaustConfig}
  **/
 export default setConfig({
   templates,
-  experimentalPlugins: [new PersistedQueriesPlugin()],
+  experimentalPlugins: [
+    new TempPersistedQueriesFixPlugin(),
+    new PersistedQueriesPlugin()
+  ],
   possibleTypes,
 });
