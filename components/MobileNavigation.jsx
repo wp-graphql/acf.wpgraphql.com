489e0881d725c09e1b26e1274578858d0b7f8785
Joe Fusco <joe.fusco@wpengine.com>
2023-09-19 14:09:26 -0400

    Fix skewed mobile nav logo
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- components/MobileNavigation.jsx
+++ components/MobileNavigation.jsx
@@ -82,7 +82,7 @@ export function MobileNavigation({ navigation }) {
               <CloseIcon className="h-6 w-6 stroke-slate-500" />
             </button>
             <Link href="/" className="ml-6" aria-label="Home page">
-              <Logo className="h-9 w-9" />
+              <Logo className="h-auto w-9" />
             </Link>
           </div>
           <Navigation navigation={navigation} className="mt-5 px-1" />
