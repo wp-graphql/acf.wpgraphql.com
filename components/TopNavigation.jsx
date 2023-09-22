dce1bb77b12a03f8824d8979f0d25decae4831af
Jason Bahl <jasonbahl@mac.com>
2023-09-22 12:57:33 -0600

    - fix href on featured subMenu item

--- components/TopNavigation.jsx
+++ components/TopNavigation.jsx
@@ -66,7 +66,7 @@ export function TopNavigation({ navigation }) {
                             <NavigationMenuLink asChild>
                               <Link
                                 className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                href="/"
+                                href={sublink.href}
                                 target={sublink?.target ?? null}
                               >
                                 {/* <Icons.logo className="h-6 w-6" /> */}
