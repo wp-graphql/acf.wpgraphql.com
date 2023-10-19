be4576a2c9264b3a35674a4dcc8b288514a6530b
Joe Fusco <joe.fusco@wpengine.com>
2023-10-19 13:02:12 -0400

    Refactor sidebar nav
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- components/DocsSidebarNavigation.jsx
+++ components/DocsSidebarNavigation.jsx
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { useState } from 'react'
 
 DocsSidebarNavigation.fragment = gql`
   fragment DocsSidebarNavigationFragment on RootQuery {
@@ -19,65 +20,94 @@ DocsSidebarNavigation.fragment = gql`
   }
 `
 
-const isActiveSection = (section, currentNode) => {
-  if (!currentNode) {
-    return false
-  }
-
-  let isActive = false
-
-  section.links.forEach((link) => {
-    if (link.href.includes(currentNode?.uri)) {
-      isActive = true
-    }
-  })
-
-  return isActive
-}
-
 export function DocsSidebarNavigation({ className, data, navigation }) {
+  const isActiveSection = (section, currentNode) => {
+    if (!currentNode) {
+      return false;
+    }
+  
+    return section.links.some(link => link.href.includes(currentNode.uri));
+  };
+
+  const [expandedSections, setExpandedSections] = useState(() => {
+    const initialExpandedSections = {};
+    navigation.forEach(section => {
+      initialExpandedSections[section.title] = isActiveSection(section, data?.node);
+    });
+    return initialExpandedSections;
+  });
+
+  const toggleSection = (sectionTitle) => {
+    setExpandedSections(prevState => ({
+      ...prevState,
+      [sectionTitle]: !prevState[sectionTitle],
+    }));
+  };
+
   return (
     <nav className={clsx('text-base lg:text-sm', className)}>
       <ul role="list" className="space-y-9">
         {navigation.map((section) => (
           <li key={section.title}>
-            <Link href={section.href}>
-              <h2
-                className={clsx(
-                  'font-display font-medium ',
-                  isActiveSection(section, data?.node)
-                    ? 'text-sky-500'
-                    : 'text-slate-900 dark:text-white',
-                )}
-              >
-                {section.title}
-              </h2>
-            </Link>
+            <div className="flex justify-between items-center">
+              <Link href={section.href}>
+                <h2 className="font-display font-medium text-slate-900 dark:text-white cursor-pointer">
+                  {section.title}
+                </h2>
+              </Link>
+              <div className="inline-flex items-center justify-center p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+                <button
+                  onClick={() => toggleSection(section.title)}
+                  className="text-slate-900 dark:text-white transition-transform duration-300"
+                >
+                  <svg
+                    className={clsx(
+                      'transform',
+                      expandedSections[section.title] && 'rotate-90',
+                      'w-4 h-4'
+                    )}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </button>
+                </div>
+            </div>
             <ul
               role="list"
-              className="mt-2 space-y-2 border-l-2 border-slate-100 dark:border-slate-800 lg:mt-4 lg:space-y-4 lg:border-slate-200"
+              className={clsx(
+                'mt-2 space-y-2 border-l-2 transition-all duration-300',
+                expandedSections[section.title]
+                  ? 'border-slate-100 dark:border-slate-800'
+                  : 'border-transparent hidden',
+                'lg:mt-4 lg:space-y-4 lg:border-slate-200'
+              )}
             >
-              {isActiveSection(section, data?.node)
-                ? section.links.map((link) => (
-                    <li key={link.href} className="relative">
-                      <Link
-                        href={link.href}
-                        className={clsx(
-                          'block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
-                          link.href === data?.node?.uri
-                            ? 'font-semibold text-sky-500 before:bg-sky-500'
-                            : 'text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300',
-                        )}
-                      >
-                        {link.title}
-                      </Link>
-                    </li>
-                  ))
-                : null}
+              {section.links.map((link) => (
+                <li key={link.href} className="relative pl-3.5">
+                  <Link
+                    href={link.href}
+                    className={clsx(
+                      'block',
+                      link.href === data?.node?.uri ? 'font-semibold text-sky-500' : 'text-slate-500 hover:text-slate-600 dark:text-slate-400'
+                    )}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </li>
         ))}
       </ul>
     </nav>
-  )
+  );
 }
