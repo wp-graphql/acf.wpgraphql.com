c1936cb42b46363a04957742db1226fd44b8f90a
Joe Fusco <joe.fusco@wpengine.com>
2023-12-01 11:00:39 -0500

    refactor field type config block
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- wp-blocks/AcfFieldTypeConfigurationBlock.js
+++ wp-blocks/AcfFieldTypeConfigurationBlock.js
@@ -1,43 +1,86 @@
 import React from 'react';
 import { gql } from '@apollo/client'
-import Image from 'next/image'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { snakeToPascalCase } from '@/lib/snakeToPascalCase';
 
 const tabData = [
   {
-    key: 'acf_ui',
-    name: 'ACF UI',
-    content: AcfUiTabContent
-  },
-  {
     key: 'php',
     name: 'PHP',
-    content: () => <p>PHP Content</p>
+    component: PHPTabContent
   },
   {
     key: 'json',
     name: 'JSON',
-    content: () => <p>JSON Content</p>
+    component: JSONTabContent
   },
 ];
 
-function AcfUiTabContent({ fieldLabel, uiScreenshot }) {
-  return (
-    <>
-      <p>Field Label: {fieldLabel}</p>
-      <Image
-        src={uiScreenshot?.node?.mediaItemUrl}
-        alt={uiScreenshot?.node?.altText}
-        width={uiScreenshot?.node?.mediaDetails?.width}
-        height={uiScreenshot?.node?.mediaDetails?.height}
-      />
-    </>
+function PHPTabContent({fieldTypeConfigurationBlockFields}) {
+  const { acfFieldType } = fieldTypeConfigurationBlockFields;
+  const phpGuts = `array(
+      'key'                   => 'group_{unique_key_1}',
+      'title'                 => 'My Field Group with ${acfFieldType}',
+      'show_in_graphql'       => 1,
+      'graphql_field_name'    => 'myFieldGroupWith${snakeToPascalCase(acfFieldType)}',
+      'map_graphql_types_from_location_rules' => 0,
+      'graphql_types'         => array( 'Page' ),
+      'fields'                => array(
+        array(
+          'key'                => 'field_{unique_key_2}',
+          'label'              => 'My Field',
+          'name'               => 'my_field',
+          'type'               => '${acfFieldType}',
+          'show_in_graphql'    => 1,
+          'graphql_field_name' => 'myField',
+        ),
+      ),
+      'location'              => array(
+        array(
+          array(
+            'param'    => 'post_type',
+            'operator' => '==',
+            'value'    => 'page',
+          ),
+        ),
+      )
+    )`
+  
+  let phpString = `<?php
+add_action( 'acf/include_fields', function() {
+  // Check if the ACF function exists
+  if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+      return;
+  }
+
+  // Add local field group
+  acf_add_local_field_group(
+    ${phpGuts}
   );
-} 
+});
+`;
+
+  return(
+    <pre>
+      {phpString}
+    </pre>
+  )
+}
+
+function JSONTabContent({fieldTypeConfigurationBlockFields}) {
+  const { acfFieldType } = fieldTypeConfigurationBlockFields;
+  
+  // const result = JSON.parse(acfFieldType);
+  // const prettyResult = JSON.stringify(result, null, 2);
+
+  return(
+    <pre>
+      JSON
+    </pre>
+  )
+}
 
 export function AcfFieldTypeConfigurationBlock({ fieldTypeConfigurationBlockFields }) {
-  const { fieldLabel, uiScreenshot } = fieldTypeConfigurationBlockFields;
-
   return (
     <Tabs defaultValue={tabData[0].key}>
       <TabsList aria-label="Dynamic Tabs">
@@ -50,7 +93,7 @@ export function AcfFieldTypeConfigurationBlock({ fieldTypeConfigurationBlockFiel
       {tabData.map(tab => (
         <TabsContent key={tab.key} value={tab.key}>
           <div className="p-4">
-            {tab.key === 'acf_ui' ? tab.content({ fieldLabel, uiScreenshot }) : tab.content()}
+            {tab.component({ fieldTypeConfigurationBlockFields })}
           </div>
         </TabsContent>
       ))}
@@ -67,17 +110,7 @@ AcfFieldTypeConfigurationBlock.fragments = {
   entry: gql`
     fragment AcfFieldTypeConfigurationBlockFragment on AcfFieldTypeConfigurationBlock {
       fieldTypeConfigurationBlockFields {
-        fieldLabel
-        uiScreenshot {
-          node {
-            mediaItemUrl
-            altText
-            mediaDetails {
-              height
-              width
-            }
-          }
-        }
+        acfFieldType
       }
     }
   `,
