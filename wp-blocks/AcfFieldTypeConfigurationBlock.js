0ea67603ad53a80febbc85e59ebfcae107971037
Joe Fusco <joe.fusco@wpengine.com>
2023-12-11 17:04:46 -0500

    Add syntax highlighting to config block
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- wp-blocks/AcfFieldTypeConfigurationBlock.js
+++ wp-blocks/AcfFieldTypeConfigurationBlock.js
@@ -1,127 +1,60 @@
-import { gql } from '@apollo/client'
+import { gql } from '@apollo/client';
 import React, { useState, useEffect } from 'react';
-
-import {
-  Card,
-  CardHeader,
-} from '@/components/ui/card';
+import { Card, CardHeader } from '@/components/ui/card';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { snakeToPascalCase } from '@/lib/snakeToPascalCase';
 import { stringToHash } from '@/lib/stringToHash';
+import { highlightCode } from '@/lib/highlightCode';
 
-// Field Type is known:
-
-// Fetch the proper JSON to register that field within a field group.
-
-// Use fetched JSON as a param for the PHP tab.
-
-// Highlight relevant lines.
-
-const tabData = [
-  {
-    key: 'php',
-    name: 'PHP',
-    component: PHPTabContent
-  },
-  {
-    key: 'json',
-    name: 'JSON',
-    component: JSONTabContent
-  },
-];
-
-function PHPTabContent({fieldTypeConfigurationBlockFields, uniqueId}) {
-  const { acfFieldType } = fieldTypeConfigurationBlockFields;
-
-  const phpGuts = `array(
-      'key'                   => 'my_field_group_${uniqueId}',
-      'title'                 => 'My Field Group with ${acfFieldType}',
-      'show_in_graphql'       => 1,
-      'graphql_field_name'    => 'myFieldGroupWith${snakeToPascalCase(acfFieldType)}',
-      'map_graphql_types_from_location_rules' => 0,
-      'graphql_types'         => array( 'Page' ),
-      'fields'                => array(
-        array(
-          'key'                => 'my_field_${uniqueId}',
-          'label'              => 'My Field',
-          'name'               => 'my_field',
-          'type'               => '${acfFieldType}',
-          'show_in_graphql'    => 1,
-          'graphql_field_name' => 'myFieldWith${snakeToPascalCase(acfFieldType)}',
-        ),
-      ),
-      'location'              => array(
-        array(
-          array(
-            'param'    => 'post_type',
-            'operator' => '==',
-            'value'    => 'page',
-          ),
-        ),
-      )
-    )`
-  
-  let phpString = `<?php
-add_action( 'acf/include_fields', function() {
-  // Check if the ACF function exists
-  if ( ! function_exists( 'acf_add_local_field_group' ) ) {
-      return;
-  }
-
-  // Add local field group
-  acf_add_local_field_group(
-    ${phpGuts}
-  );
-});
-`;
-
-  return(
-    <pre className='mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border py-4'>
-      {phpString}
-    </pre>
-  )
-}
-
-function JSONTabContent({fieldTypeConfigurationBlockFields, uniqueId}) {
-  const { acfFieldType } = fieldTypeConfigurationBlockFields;
-
-  const jsonData = {
+function generateData(uniqueId, acfFieldType) {
+  return {
     key: `my_field_group_${uniqueId}`,
-    title:`My Field Group with ${acfFieldType}`,
+    title: `My Field Group with ${acfFieldType}`,
     show_in_graphql: 1,
     graphql_field_name: `myFieldGroupWith${snakeToPascalCase(acfFieldType)}`,
     map_graphql_types_from_location_rules: 0,
-    graphql_types: [ 
-      "Page"
-    ],
-    fields: [
-      {
-        key: `my_field_${uniqueId}`,
-        label: "My Field",
-        name: "my_field",
-        type: `${acfFieldType}`,
-        show_in_graphql: 1,
-        graphql_field_name: `myFieldWith${snakeToPascalCase(acfFieldType)}`,
-      },
-    ],
-    'location': [
-      [
-        {
-          param: 'post_type',
-          operator:'==',
-          value: 'page',
-        },
-      ],
-    ]
+    graphql_types: ['Page'],
+    fields: [{
+      key: `my_field_${uniqueId}`,
+      label: 'My Field',
+      name: 'my_field',
+      type: `${acfFieldType}`,
+      show_in_graphql: 1,
+      graphql_field_name: `myFieldWith${snakeToPascalCase(acfFieldType)}`,
+    }],
+    location: [[{
+      param: 'post_type',
+      operator: '==',
+      value: 'page',
+    }]],
   };
+}
 
-  const jsonString = JSON.stringify(jsonData, null, 2);
+function generatePHPTabContent(data) {
+  const phpString = `<?php
+add_action( 'acf/include_fields', function() {
+  if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+      return;
+  }
+  acf_add_local_field_group(
+    [
+      ${JSON.stringify(data, null, 2).replace(/\n/g, "\n      ") /* Fix rendered indentation */}
+    ]
+  );
+});`;
+  return highlightCode(phpString, "php", [3, 4, 5]);
+}
 
-  return(
-    <pre className='mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border py-4'>
-      {jsonString}
-    </pre>
-  )
+function generateJSONTabContent(data) {
+  const jsonString = JSON.stringify(data, null, 2);
+  return highlightCode(jsonString, "json", [3, 4, 5]);
+}
+
+function TabContent({ fieldTypeConfigurationBlockFields, uniqueId, format }) {
+  const { acfFieldType } = fieldTypeConfigurationBlockFields;
+  const data = generateData(uniqueId, acfFieldType);
+
+  return format === 'php' ? generatePHPTabContent(data) : generateJSONTabContent(data);
 }
 
 export function AcfFieldTypeConfigurationBlock({ fieldTypeConfigurationBlockFields }) {
@@ -130,7 +63,12 @@ export function AcfFieldTypeConfigurationBlock({ fieldTypeConfigurationBlockFiel
 
   useEffect(() => {
     setUniqueId(stringToHash(acfFieldType));
-  }, []);
+  }, [acfFieldType]);
+
+  const tabData = [
+    { key: 'php', name: 'PHP' },
+    { key: 'json', name: 'JSON' },
+  ];
 
   return (
     <Card>
@@ -145,14 +83,14 @@ export function AcfFieldTypeConfigurationBlock({ fieldTypeConfigurationBlockFiel
           </TabsList>
           {tabData.map(tab => (
             <TabsContent key={tab.key} value={tab.key}>
-              {tab.component({ fieldTypeConfigurationBlockFields, uniqueId })}
+              <TabContent fieldTypeConfigurationBlockFields={fieldTypeConfigurationBlockFields} uniqueId={uniqueId} format={tab.key} />
             </TabsContent>
           ))}
         </Tabs>
       </CardHeader>
     </Card>
   );
-};
+}
 
 AcfFieldTypeConfigurationBlock.displayName = `AcfFieldTypeConfigurationBlock`
 AcfFieldTypeConfigurationBlock.config = {
