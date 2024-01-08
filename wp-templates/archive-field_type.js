79b713aeb41301e4d713aba157501bfefbb06915
Joe Fusco <joe.fusco@wpengine.com>
2024-01-08 16:04:00 -0500

    Implement Faust’s multiple queries
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- wp-templates/archive-field_type.js
+++ wp-templates/archive-field_type.js
@@ -1,39 +1,19 @@
 import { gql } from '@apollo/client'
 import Head from 'next/head'
+import { useFaustQuery } from '@faustwp/core';
 
 import { FieldTypesList } from '@/components/FieldTypesList'
 import { LayoutArchive } from '@/components/LayoutArchive'
 
-export const ArchiveFieldType = (props) => {
-  const { data } = props
-
-  const { node } = data
-
-  if (!node) {
-    return null
+export const GET_LAYOUT_QUERY = gql`
+  query GetLayout {
+    ...LayoutArchiveFragment
   }
+  ${LayoutArchive.fragment}
+`;
 
-  let toc = []
-
-  return (
-    <>
-      <Head>
-        <title>{`${data?.node?.label} - WPGraphQL for ACF`}</title>
-      </Head>
-      <LayoutArchive
-        title={data?.node?.label ? data.node.label : 'WPGraphQL for ACF'}
-        data={data}
-        navigation={data?.navigation?.nodes}
-        toc={toc}
-      >
-        <FieldTypesList data={data} />
-      </LayoutArchive>
-    </>
-  )
-}
-
-ArchiveFieldType.query = gql`
-  query GetArchiveFieldType($uri: String!) {
+export const GET_POST_QUERY = gql`
+  query GetPost($uri: String!) {
     node: nodeByUri(uri: $uri) {
       __typename
       uri
@@ -62,9 +42,49 @@ ArchiveFieldType.query = gql`
         }
       }
     }
-    ...LayoutArchiveFragment
   }
-  ${LayoutArchive.fragment}
-`
+`;
+
+export const ArchiveFieldType = (props) => {
+  const { data } = props
+console.log({data});
+  const { node } = useFaustQuery(GET_POST_QUERY);
+  const { primaryMenuItems, footerMenuItems, docsSidebarMenuItems } = useFaustQuery(GET_LAYOUT_QUERY);
+
+  if (!node) {
+    return null
+  }
+
+  let toc = []
+
+  console.log({primaryMenuItems, footerMenuItems, docsSidebarMenuItems});
+
+
+  return (
+    <>
+      <Head>
+        <title>{`${data?.node?.label} - WPGraphQL for ACF`}</title>
+      </Head>
+      <LayoutArchive
+        title={data?.node?.label ? data.node.label : 'WPGraphQL for ACF'}
+        data={data}
+        navigation={data?.navigation?.nodes}
+        toc={toc}
+      >
+        <FieldTypesList data={data} />
+      </LayoutArchive>
+    </>
+  )
+}
+
+ArchiveFieldType.queries = [
+  {
+    query: GET_LAYOUT_QUERY,
+  },
+  {
+    query: GET_POST_QUERY,
+    variables: ({ uri }) => ({ uri }),
+  }
+];
 
 ArchiveFieldType.variables = ({ uri }) => ({ uri })
