b9d2cbdb6c0b734180eb523c8131a2bfd7145474
Jason Bahl <jasonbahl@mac.com>
2024-01-11 10:41:07 -0700

    - implement ISR support according to: https://docs.google.com/document/d/1F1LTYmeHXD5-Zr6ParK1zRUjaryoYHG4MchOd_FCt-c/edit#heading=h.zf6klsmn20w5

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,25 @@ const nextConfig = {
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
     experimental: {
         scrollRestoration: true,
+        incrementalCacheHandlerPath: atlas_cache_handler(),
+        isrMemoryCacheSize: 0
     },
     trailingSlash: true,
     images: {
         domains: [getWpHostname()],
     },
 };
+
+function atlas_cache_handler() {
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
+      return {
+        incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
+        isrMemoryCacheSize: 0
+      }
+    }
+  
+    return undefined
+  }
+  
 
 export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
