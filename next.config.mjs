056a5f149b620a25375452680767b4960d27f419
Jason Bahl <jasonbahl@mac.com>
2024-01-11 10:58:28 -0700

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,24 @@ import { withFaust, getWpHostname } from '@faustwp/core';
 import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
 
+function experimentalConfig() {
+
+    const experimental = {
+        scrollRestoration: true,
+    }
+
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED === undefined) {
+        const atlasExperimentalOptions = { ...experimental, ...{
+            incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
+            isrMemoryCacheSize: 0
+        }}
+       console.log( { atlasExperimentalOptions })
+       return atlasExperimentalOptions
+    }
+  
+    return experimental
+}
+  
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
@@ -12,23 +30,7 @@ const nextConfig = {
     },
 };
 
-function experimentalConfig() {
 
-    const experimental = {
-        scrollRestoration: true,
-    }
-
-    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
-        const atlasExperimentalOptions = { ...experimental, ...{
-            incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
-            isrMemoryCacheSize: 0
-        }}
-       console.log( { atlasExperimentalOptions })
-       return atlasExperimentalOptions
-    }
-  
-    return experimental
-  }
   
 
 export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
