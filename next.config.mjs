b4a9a765a0ebdd3ad0ca414d41a27f540af6ab9c
Jason Bahl <jasonbahl@mac.com>
2024-01-12 15:34:55 -0700

    - revert next.config.mjs to not include Atlas Cache Handler support

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,16 @@ import { withFaust, getWpHostname } from '@faustwp/core';
 import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
 
-function experimentalConfig() {
-
-    const experimental = {
-        scrollRestoration: true,
-    }
-
-    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
-        const atlasExperimentalOptions = { ...experimental, ...{
-            // instead of require.resolve, use createRequire 
-            // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import.meta/resolve
-            incrementalCacheHandlerPath: require.resolve( './.atlas/atlas-cache-handler.js' ),
-            isrMemoryCacheSize: 0
-        }}
-        return atlasExperimentalOptions
-    }
-  
-    return experimental
-}
-  
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-    experimental: experimentalConfig(),
+    experimental: {
+        scrollRestoration: true,
+    },
     trailingSlash: true,
     images: {
         domains: [getWpHostname()],
     },
 };
-  
+
 export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
