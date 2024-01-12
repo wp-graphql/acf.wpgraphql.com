34691b5cc3165c7e007215ccc25c52d9b0cd28fe
Jason Bahl <jasonbahl@mac.com>
2024-01-12 16:13:28 -0700

    - try ISR one more time

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,33 @@
 import { withFaust, getWpHostname } from '@faustwp/core';
 import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
+import atlasCacheHandler from './.atlas/atlas-cache-handler.js'
 
+function experimentalConfig() {
+
+    const experimental = {
+        scrollRestoration: true,
+    }
+
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
+        const atlasExperimentalOptions = { ...experimental, ...{
+            incrementalCacheHandlerPath: atlasCacheHandler,
+            isrMemoryCacheSize: 0
+        }}
+        return atlasExperimentalOptions
+    }
+  
+    return experimental
+}
+  
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-    experimental: {
-        scrollRestoration: true,
-    },
+    experimental: experimentalConfig(),
     trailingSlash: true,
     images: {
         domains: [getWpHostname()],
     },
 };
-
+  
 export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
