13c1ea187bb5e7f5a49c8a7095bba9473edc4f0f
Jason Bahl <jasonbahl@mac.com>
2024-01-12 19:21:32 -0700

    - refactor. Thanks @jordanmaslyn! 🙌

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,34 @@
 import { withFaust, getWpHostname } from '@faustwp/core';
 import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
-import atlasCacheHandler from './.atlas/atlas-cache-handler.js'
 
-function experimentalConfig() {
-
-    const experimental = {
-        scrollRestoration: true,
+async function getAtlasCacheHandler() {
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== "true") {
+        return {};
     }
+    
+    const {default: atlasCacheHandler} = await import('./.atlas/atlas-cache-handler.js');
 
-    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
-        const atlasExperimentalOptions = { ...experimental, ...{
-            incrementalCacheHandlerPath: atlasCacheHandler,
-            isrMemoryCacheSize: 0
-        }}
-        return atlasExperimentalOptions
-    }
-  
-    return experimental
+    return {
+        incrementalCacheHandlerPath: atlasCacheHandler,
+        isrMemoryCacheSize: 0,
+    };
 }
   
-const nextConfig = {
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-    experimental: experimentalConfig(),
-    trailingSlash: true,
-    images: {
-        domains: [getWpHostname()],
-    },
+const nextConfig = async () => {
+    return {
+        reactStrictMode: true,
+        pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+        experimental: {
+            scrollRestoration: true,
+            ...(await getAtlasCacheHandler())
+        },
+        trailingSlash: true,
+        images: {
+            domains: [getWpHostname()],
+        },
+    }
 };
-  
+
+
 export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
