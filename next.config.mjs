17e293826d1e78e6ef8cf4947e88e7c03df8e6db
Jason Bahl <jasonbahl@mac.com>
2024-01-12 19:50:40 -0700

    - modify config

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,46 @@ import { withFaust, getWpHostname } from '@faustwp/core';
 import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
 
-async function getAtlasCacheHandler() {
-    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== "true") {
-        return {};
+const getAtlasCacheHandler = async ( config = {} ) => {
+    if (process?.env?.ATLAS_CACHE_HANDLER_ENABLED && process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined ) {
+        return config;
     }
     
-    const {default: atlasCacheHandler} = await import('./.atlas/atlas-cache-handler.js');
+    const { atlasCacheHandler } = await import('./.atlas/atlas-cache-handler.js');
 
-    return {
+    return { ...config, ...{
         incrementalCacheHandlerPath: atlasCacheHandler,
         isrMemoryCacheSize: 0,
-    };
+    } };
 }
   
-const nextConfig = async () => {
+const nextConfig = () => {
     return {
         reactStrictMode: true,
         pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-        experimental: {
+        experimental: getAtlasCacheHandler({
             scrollRestoration: true,
-            ...(await getAtlasCacheHandler())
-        },
+        }),
         trailingSlash: true,
         images: {
-            domains: [getWpHostname()],
+            domains: [ getWpHostname() ],
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'bpacfwpgraphql.wpengine.com',
+                    port: '',
+                    pathname: '/**',
+                  },
+            ]
         },
     }
 };
 
 
-export default withFaust( withSearch( withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig) ) );
+export default withFaust( 
+    withSearch( 
+        withMarkdoc({
+            schemaPath: './src/markdoc' 
+        })( nextConfig() ) 
+    ) 
+);
