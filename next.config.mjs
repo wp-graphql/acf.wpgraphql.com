7c3cab0c02b05c08a7fb90c57254c4d93c702fb8
Jason Bahl <jasonbahl@mac.com>
2024-01-16 08:37:52 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,14 @@ import withMarkdoc from '@markdoc/next.js'
 import withSearch from './markdoc/search.mjs'
 
 const getAtlasCacheHandler = async ( config = {} ) => {
-    if (process.env.ATLAS_CACHE_HANDLER_ENABLED === undefined) {
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED === undefined || process.env.ATLAS_CACHE_HANDLER_ENABLED !== 'true') {
         return { ...config };
     }
     
+    const { atlasCacheHandler } = await import('./.atlas/atlas-cache-handler.js');
+
     return { ...config, ...{
-        incrementalCacheHandlerPath: './.atlas/atlas-cache-handler.js',
+        incrementalCacheHandlerPath: atlasCacheHandler,
         isrMemoryCacheSize: 0,
     } };
 }
