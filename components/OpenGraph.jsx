d631c5cbdea1c45c5b2a989bf7ff9a1110ad38be
Joe Fusco <joe.fusco@wpengine.com>
2024-07-24 14:23:05 -0400

    Refactor defaults

--- components/OpenGraph.jsx
+++ components/OpenGraph.jsx
@@ -3,38 +3,38 @@ import Head from 'next/head'
 
 const OpenGraph = ({ seo } ) => {
 
-    const {
-        metaDesc = '',
-        opengraphDescription = '',
-        opengraphImage       = null,
-        opengraphTitle    = '',
-        opengraphType    = '',
-        opengraphUrl   = '',
-        title    = '',
-        twitterDescription   = '',
-        twitterTitle  = '',
-    } = seo
+  const {
+    metaDesc = '',
+    opengraphDescription = '',
+    opengraphImage = { sourceUrl: '/default-og-image.jpg' },
+    opengraphTitle = '',
+    opengraphType = 'website',
+    opengraphUrl = '',
+    title = '',
+    twitterDescription = '',
+    twitterTitle = '',
+  } = seo || {}; // Ensure seo is not null or undefined
+  
 
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={metaDesc} />
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={metaDesc} />
 
-                <meta property="og:url" content={opengraphUrl} />
-                <meta property="og:type" content={opengraphType} />
-                <meta property="og:title" content={opengraphTitle} />
-                <meta property="og:description" content={opengraphDescription} />
-                <meta property="og:image" content={opengraphImage?.sourceUrl || '/default-og-image.jpg'} />
+        <meta property="og:url" content={opengraphUrl} />
+        <meta property="og:type" content={opengraphType} />
+        <meta property="og:title" content={opengraphTitle} />
+        <meta property="og:description" content={opengraphDescription} />
+        <meta property="og:image" content={opengraphImage?.sourceUrl} />
 
-                <meta property="twitter:url" content={opengraphUrl} />
-                <meta name="twitter:title" content={twitterTitle} />
-                <meta name="twitter:description" content={twitterDescription} />
-                <meta name="twitter:image" content={opengraphImage?.sourceUrl || '/default-og-image.jpg'} />
-
-            </Head>
-        </>
-    )
+        <meta property="twitter:url" content={opengraphUrl} />
+        <meta name="twitter:title" content={twitterTitle} />
+        <meta name="twitter:description" content={twitterDescription} />
+        <meta name="twitter:image" content={opengraphImage?.sourceUrl} />
+      </Head>
+    </>
+  )
 }
 
 OpenGraph.fragments = {
